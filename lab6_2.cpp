3c8f21353dc0b298007ce20e995546882b83e0e8
Apithep Piyaphiphathanamongkol <kaoapithepp@Apitheps-MacBook-Pro.local>
2020-01-17 15:49:00 +0700

    Stone age

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -16,3 +16,39 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watch Star Wars VII 
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/ 
 */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string name,movie_name,dayFree,typeForm;
+    int ID,GEAR;
+    
+
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?" << endl;
+    cout << "?????: ";
+    getline(cin,name);
+    cout << "Fahsai: Wow!!! " << name << " that is a really cool name." << endl;
+    cout << "Fahsai: I think you are an engineering student. What is your student ID?" << endl;
+    cout << name << ": ";
+    cin >> ID;
+    cin.ignore();
+
+    GEAR = (ID/10000000) - 12;
+
+    cout << "Fahsai: I think you may be GEAR " << GEAR << ". I have free movie tickets for you." << endl;
+    cout << "Fahsai: Let's go to cinema together!!!" << endl;
+    cout << "Fahsai: What movie do you want to watch?" << endl;
+    cout << name << ": ";
+    getline(cin,movie_name);
+    cout << "Fahsai: So...Which day are you free to go with me?" << endl;
+    cout << name << ": ";
+    getline(cin,dayFree);
+    cout << "Fahsai: " << dayFree << "...that is OK!!! I'm looking forward to watch " << movie_name << " with you." << endl;
+    cout << name << ": ";
+    getline(cin,typeForm);
+    cout << "Fahsai: 555+ see you " << dayFree << ". Bye Bye \(^ ^)/";
+
+}
